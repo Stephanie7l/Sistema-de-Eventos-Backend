4d39143b354002267ca01adada30f11ffbcde045
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2024-06-19 18:48:12 -0300

    Create EventsModel.js

---
+++ src/Models/EventsModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const EventsSchema = new Schema({
+  title : { type: String, unique: true, },
+  picture : String,
+  description : String,
+  category : String,
+});
+
+const EventsModel = mongoose.model("events", EventsSchema);
+
+module.exports = EventsModel;
