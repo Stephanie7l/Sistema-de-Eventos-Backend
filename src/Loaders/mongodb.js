c357f3b131ca6fa8762d93251ac414a60b90c74d
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2024-06-19 18:47:18 -0300

    Add files via upload

---
+++ src/Loaders/mongodb.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function startDB(){
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+}
+
+module.exports = startDB;
